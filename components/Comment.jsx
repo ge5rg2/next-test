7db75eb65128f621150ce51a50e011185de9299d
ge5rg2 <kghee9612@gmail.com>
2023-05-11 14:35:24 +0900

    show commetns

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 
 export default function Comment({ parentData }) {
   let [comment, setComment] = useState("");
-  const [comments, setComments] = useState([]);
+  let [comments, setComments] = useState([]);
 
   // client side에서 server 의 data를 가져오고 싶다면 다음과 같이 useEffect에서 ajax
   useEffect(() => {
     fetch(`/api/comments?uid=${parentData.params.uid}`)
       .then((r) => r.json())
-      .then((result) => setComments([result, ...comment]));
+      .then((result) => setComments(result));
   }, []);
 
   return (
@@ -47,9 +47,34 @@ export default function Comment({ parentData }) {
       >
         Forwarding comments
       </button>
-      {comments.map((el, index) => {
-        <div key={index}>{el.comment}</div>;
-      })}
+      <div>
+        {comments.length > 0
+          ? comments.map((el, index) => {
+              <div key={index}>{el.email}</div>;
+            })
+          : "No comment"}
+      </div>
+      <div>
+        {comments.length > 0
+          ? comments.map((el, index) => <div key={index}>{el.comment}</div>)
+          : "No comment"}
+      </div>
     </div>
   );
 }
+
+/**
+ * <div>
+        {comments.length > 0
+          ? comments.map((el, index) => {
+              <div key={index}>{el.email}</div>;
+            })
+          : "No comment"}
+      </div>
+      {}로 dom을 씌우면 작동안됨  JSX 요소가 맵 함수 콜백 내에서 올바르게 반환되도록 합니다.
+      <div>
+        {comments.length > 0
+          ? comments.map((el, index) => <div key={index}>{el.comment}</div>)
+          : "No comment"}
+      </div>
+ */
